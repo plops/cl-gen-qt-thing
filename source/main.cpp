14e26296d5e21ccdf0fe6cceadd05533dd06430a
Martin Kielhorn <martin.kielhorn@tassinternational.com>
2017-03-30 18:18:16 +0200

    rotate rect2

--- source/main.cpp
+++ source/main.cpp
@@ -19,9 +19,18 @@ int main(int argc, char **argv) {
 
     {
       QGraphicsRectItem *rect = new QGraphicsRectItem(50, 50, 100, 100);
+      QGraphicsRectItem *rect2 = new QGraphicsRectItem(48, 48, 104, 104);
 
       rect->setFlag(QGraphicsItem::ItemIsSelectable);
       scene->addItem(rect);
+      scene->addItem(rect2);
+      {
+        auto tr = QTransform();
+
+        tr.rotate(45, Qt::ZAxis);
+        rect2->setTransform(tr);
+      }
+
       scene->addText("hello");
     }
 
